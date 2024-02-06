361ecd15c52cb0c3c3f541502ef548ae0e96117f
punyapatkha <punyatpat.kha@gmail.com>
2024-02-06 12:43:07 +0700

    multipage

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import dash
+# blog for deploy
+# https://community.plotly.com/t/migrating-from-heroku-how-to-use-render-to-deploy-a-python-dash-app-solution/68048
+
+# example code for deploy
+# https://github.com/thusharabandara/dash-app-render-deployment/blob/main/app.py
+
+
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash(__name__)
-server = app.server
+app = Dash(__name__, pages_folder='pages' , use_pages=True)
+# server = app.server
 
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+app.layout = html.Div(
+    [
+        # main app framework
+        html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
+        html.Div([
+            dcc.Link(page['name']+"  |  ", href=page['path'])
+            for page in dash.page_registry.values()
+        ]),
+        html.Hr(),
+
+        # content of each page
+        dash.page_container
+    ]
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+
+# app.layout = html.Div([
+#     html.H1('Multi-page app with Dash Pages'),
+#     html.Div([
+#         html.Div(
+#             dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+#         ) for page in dash.page_registry.values()
+#     ]),
+#     dash.page_container
+# ])
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+
+    # https://stackoverflow.com/questions/62378947/hot-reload-in-dash-does-not-automatically-update
+    # around 2 sec after save
+
+    app.run(debug=True)
+    # what diff? run & run_server
+    # app.run_server(debug=True)
---
+++ pages/home3.py
@@ -0,0 +1,15 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+
+dash.register_page(__name__,path='/home3')
+
+df = px.data.tips()
+
+layout = html.Div(
+    [
+        dcc.RadioItems([x for x in df.day.unique()], id='day-choice'),
+        dcc.Graph(id='bar-fig',
+                  figure=px.bar(df, x='smoker', y='total_bill'))
+    ]
+)
