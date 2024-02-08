d699ad98269626b193f63b0ef67849a64bcc5162
punyapatkha <punyatpat.kha@gmail.com>
2024-02-08 15:41:14 +0700

    add requirement txt , add graph from database

--- pages/home.py
+++ pages/home.py
@@ -1,11 +1,15 @@
 import dash
-from dash import dcc, html
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 from sqlalchemy import create_engine
-import pandas as pd
+from dash_bootstrap_templates import load_figure_template
+
+load_figure_template(["cyborg", "darkly"])
 from utils.db import printa
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 
 valueENV = os.getenv("DBconnection") 
 if valueENV is None:
@@ -22,8 +26,34 @@ else:
 
 dash.register_page(__name__,path='/',name="Sales Summary 📋 ")
 
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+# fig2 = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group", template='darkly')
+
+
+df = pd.read_sql_query("select sum(total_price) as total_price,branch_id,employee_id from transaction group by branch_id,employee_id", engine)
+fig2 = px.bar(df, x="employee_id", y="total_price", color="branch_id", barmode="group", template='darkly')
+
+
+df = pd.read_sql_query("select * from transaction", engine)
+fig = px.scatter(df, x="tran_date", y="total_price", template='darkly')
+
+
+
 layout = html.Div(
     [
-        dcc.Markdown('# This will be the content of Page 1'+printa("a"))
+        dcc.Markdown('# This will be the content of Page 1'+printa("a")),
+        dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig2
+    )
     ]
 )
